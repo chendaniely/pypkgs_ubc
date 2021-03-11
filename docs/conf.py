c58d0b700de7fce83ade6fcf712ae04c7a227daa
ttimbers <tiffany.timbers@stat.ubc.ca>
2021-03-10 21:45:16 -0800

    removed uneeded dev deps

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,7 @@ author = u"Tiffany Timbers"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon',
-              'nbsphinx']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
---
+++ docs/source/pypkgs_ubc.rst
@@ -0,0 +1,21 @@
+pypkgs\_ubc package
+===================
+
+Submodules
+----------
+
+pypkgs\_ubc.pypkgs\_ubc module
+------------------------------
+
+.. automodule:: pypkgs_ubc.pypkgs_ubc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: pypkgs_ubc
+   :members:
+   :undoc-members:
+   :show-inheritance:
